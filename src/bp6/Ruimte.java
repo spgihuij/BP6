4dfc6e7fa528f9fe4d3267d292b233b624b09e34
spgihuij <servihuijbregts@gmail.com>
2019-11-13 10:06:18 +0100

    Class Ruimte

--- src/bp6/Ruimte.java
+++ src/bp6/Ruimte.java
@@ -1,4 +1,28 @@
 package bp6;
 
 public class Ruimte {
+
+    protected String id;
+    private String lampen;
+
+    public Ruimte(String id, String lampen) {
+        this.id = id;
+        this.lampen = lampen;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLampen() {
+        return lampen;
+    }
+
+    public void setLampen(String lampen) {
+        this.lampen = lampen;
+    }
 }
