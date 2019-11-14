c1b6f44d13c711ae3786786bc76018a2e6071d77
spgihuij <servihuijbregts@gmail.com>
2019-11-14 22:44:25 +0100

    Push voor morgen op school

--- src/bp6/Weer.java
+++ src/bp6/Weer.java
@@ -1,4 +1,39 @@
 package bp6;
 
+import java.time.LocalDateTime;
+
 public class Weer {
+    private LocalDateTime tijd;
+    private double temperatuur;
+    private String beschrijving;
+
+    public Weer(LocalDateTime tijd, double temperatuur, String beschrijving) {
+        this.tijd = tijd;
+        this.temperatuur = temperatuur;
+        this.beschrijving = beschrijving;
+    }
+
+    public LocalDateTime getTijd() {
+        return tijd;
+    }
+
+    public void setTijd(LocalDateTime tijd) {
+        this.tijd = tijd;
+    }
+
+    public double getTemperatuur() {
+        return temperatuur;
+    }
+
+    public void setTemperatuur(double temperatuur) {
+        this.temperatuur = temperatuur;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
 }
--- src/bp6/WeerView.java
+++ src/bp6/WeerView.java
@@ -1,4 +1,22 @@
 package bp6;
 
-public class WeerView {
+import java.awt.*;
+
+public class WeerView  extends ContainerView {
+private Label lblZonkracht;
+private Label lblTemperatuur;
+    private Label lblVoorspelling;
+    private Label lblAdvies;
+
+    private ContainerView containerView;
+
+    public WeerView(){
+        containerView = new ContainerView();
+        lblAdvies = new Label();
+        lblTemperatuur = new Label();
+        lblVoorspelling = new Label();
+        lblZonkracht = new Label();
+
+    }
+
 }
