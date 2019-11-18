0f449bdc62f0d393b423200f1a609e53cf416bad
spgihuij <servihuijbregts@gmail.com>
2019-11-18 09:36:02 +0100

    Main

--- src/bp6/Main.java
+++ src/bp6/Main.java
@@ -1,4 +1,27 @@
 package bp6;
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    private Pane ContainerView;
+
+    public void start(Stage primaryStage){
+        Pane mainPane = new Pane();
+        mainPane.getChildren().add(ContainerView);
+        Scene scene = new Scene(ContainerView, 800, 800);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("BP6");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
